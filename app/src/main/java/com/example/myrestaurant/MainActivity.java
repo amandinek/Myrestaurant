84a7517d32771ecd119722a3a35426a9b739de3d
amandinek <karambiziga@gmail.com>
2019-10-15 15:05:06 +0200

    first commit

---
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findRestaurantButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+//    GridView gridView;
+//    String[] letters = new String[] {
+//            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+//        Typeface typeface = Typeface.createFromAsset(getAssets(), "font/delighter.otf");
+        mFindRestaurantsButton.setOnClickListener(this);
+//
+//        gridView = (GridView) findViewById(R.id.baseGridView);
+//        gridView.setAdapter(new AlphabetAdapter(this, letters,typeface));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+}
